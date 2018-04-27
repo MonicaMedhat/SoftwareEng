c1d42bc644ba46f9a50a1407d745b31398eb73b8
MonicaMedhat <30467988+MonicaMedhat@users.noreply.github.com>
2018-04-27 19:45:22 +0200

    Add files via upload
    email,passwords,approve Models

---
+++ Mapprove.php
@@ -0,0 +1,58 @@
+<?php
+require_once("db.php");
+ $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+
+class Approve
+{
+    public $ID;
+	public $App_ID ;
+	public $TransactionLog_ID;
+	
+    
+     public function insert(){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+       
+         $sql = "INSERT INTO  `approve`( `App_ID`, `TransactionLog_ID`) VALUES ('".$this->App_ID."' , '".$this->TransactionLog_ID."')";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+    
+     public function Delete($ID){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+       
+        $sql="DELETE FROM approve WHERE ID ='".$ID."'";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+     public function update($ID){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+         
+       
+         $sql = "UPDATE approve SET `App_ID`='".$this->App_ID."',`TransactionLog_ID`='".$this->TransactionLog_ID."' WHERE ID ='".$ID."'";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+    
+}
+?>
---
+++ Mpassword.php
@@ -0,0 +1,58 @@
+<?php
+require_once("db.php");
+ $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+
+class Passwords
+{
+    public $ID;
+	public $User_ID ;
+	public $Value;
+	
+    
+     public function insert(){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+       
+         $sql = "INSERT INTO  `passwords`( `User_ID`, `Value`) VALUES ('".$this->User_ID."' , '".$this->Value."')";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+    
+     public function Delete($ID){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+       
+        $sql="DELETE FROM passwords WHERE ID ='".$ID."'";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+     public function update($ID){
+        $db_obj = new dbconnect;
+		$con = $db_obj->connect();
+        $con->set_charset("utf8");
+        header('Content-Type: text/html; charset=utf-8');
+         
+       
+         $sql = "UPDATE passwords SET `User_ID`='".$this->User_ID."',`Value`='".$this->Value."' WHERE ID ='".$ID."'";
+         
+        $db_obj->connect();
+        $db_obj->executesql($sql);
+        $db_obj->disconnect();
+    }
+    
+    
+}
+?>
